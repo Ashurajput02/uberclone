e690c2a4d6d404ef975c569c77311ac274d3ecd3
Ashu Rajput <ashurajput7017@gmail.com>
2025-06-01 18:10:45 +0530

    set up server

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const app=require('./app');
+const http=require('http');
+port=process.env.PORT || 3000;
+const server=http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
